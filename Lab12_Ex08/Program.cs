2f3c3bbf780a9a7a4bb118dfe27845cfb3885488
PhumAkrthum <66030270@kmitl.ac.th>
2025-02-23 17:21:40 +0700

    update8-10

---
+++ Lab12_Ex08/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+DerivedClass dc = new DerivedClass();
+System.Console.WriteLine(dc.Hello);
+
+class BaseClass
+{
+    public string Hello = "Hello From BaseClass";
+}
+class DerivedClass : BaseClass
+{
+    public string Hello = "Hello From DerivedClass";
+}
---
+++ Lab12_Ex10/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+DerivedClass dc = new DerivedClass();
+Console.WriteLine("Use Type Casting or Data Type Conversion");
+((BaseClass)dc).Greeting(); 
+
+Console.WriteLine("------------");
+Console.WriteLine("Use as keyword");
+(dc as BaseClass).Greeting();
+
+class BaseClass
+{
+    public string Hello = "Hello From BaseClass";
+    public void Greeting()
+    {
+        Console.WriteLine("This is method in BaseClass");
+        Console.WriteLine(Hello);
+    }
+
+}
+class DerivedClass : BaseClass
+{
+    public string Hello = "Hello From DerivedClass";
+    public void Greeting()
+    {
+        Console.WriteLine("This is method in DerivedClass");
+        Console.WriteLine(Hello);
+    }
+}
